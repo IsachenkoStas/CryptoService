fc22a78a623ea793cc5c515615cfc82be61d28ba
allencooper <isachello77@gmail.com>
2023-11-23 18:13:18 +0300

    added liquibase files

--- src/main/java/com/example/cryptoservice/CryptoServiceApplication.java
+++ src/main/java/com/example/cryptoservice/CryptoServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CryptoServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CryptoServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoServiceApplication.class, args);
+    }
 }
